50f2b447945182e09866c5146bcf7820a6450365
vitorararuna <contato.vitorararuna@gmail.com>
2021-09-22 18:26:16 -0300

    add estilos globais + fontes personalisadas

--- App.jsx
+++ App.jsx
@@ -1,10 +1,27 @@
-// import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 
-import { Dashboard } from './src/screens/';
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { Dashboard } from './src/screens/Dashboard';
+
+import AppLoading from 'expo-app-loading';
+import theme from './src/global/styles/theme';
+import { useFonts, Poppins_400Regular, Poppins_500Medium, Poppins_700Bold } from '@expo-google-fonts/poppins'
 
 export default function App() {
+
+  const [fontsLoader] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  })
+
+  if (!fontsLoader) {
+    return <AppLoading />
+  }
+
   return (
-    <Dashboard />
+    <ThemeProvider theme={theme}>
+      <Dashboard />
+    </ThemeProvider>
   );
 }
