6859c60f168ac3c4e883c186435ed1e484371353
vitorararuna <contato.vitorararuna@gmail.com>
2021-09-22 17:41:27 -0300

    add typescript

---
+++ App.jsx
@@ -0,0 +1,10 @@
+// import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+
+import { Dashboard } from './src/screens/';
+
+export default function App() {
+  return (
+    <Dashboard />
+  );
+}
